5fe986f5e9671d8aae205f67fe6dfbf0d02dc734
Samit Koyom <samitkoyom@gmail.com>
2025-04-19 23:21:05 +0700

    update path css

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@section('title') | Backend Laravel @show</title>
 
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    {{-- @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot'))) --}}
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
+    {{-- @endif --}}
 
     @yield('styles')
 </head>
